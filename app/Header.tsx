1a8b71a803c1128231cc41ef61545dd1fe6b5ca2
JavaidAhmadKhan <relaxjavaid@gmail.com>
2022-12-10 13:15:08 +0530

    intialised project with  nextjs, tailwind typscript

---
+++ app/Header.tsx
@@ -0,0 +1,26 @@
+import { Bars3Icon } from "@heroicons/react/24/solid"
+import Link from "next/link"
+import NavLinks from "./NavLinks";
+
+
+const Header = () => {
+    return (
+        <header>
+            <div className="grid grid-cols-3 p-10 items-center">
+                <Bars3Icon className="w-8 h-8 cursor-pointer" />
+                <Link href="/" prefetch={false}>
+                    <h1 className="font-serif text-4xl text-center">News {" "}
+                        <span className="underline decoration-6 decoration-orange-400">Clever Coder </span>
+                    </h1>
+                </Link>
+                <div className="flex items-center justify-end space-x-2">
+                    {/* DarkModeButton */}
+                    <button className='hidden md:inline bg-slate-900 text-white px-4 lg:px-8 py-2 lg:py-4 rounded-full dark:bg-slate-800'>Subscribe Now</button>
+                </div>
+            </div>
+            <NavLinks />
+        </header>
+    )
+}
+
+export default Header;
---
+++ app/page.tsx
@@ -0,0 +1,8 @@
+
+const Homepage = () => {
+    return (
+        <div>page</div>
+    )
+}
+
+export default Homepage
